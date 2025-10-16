5369ab468b0f725b8034cd3d26a5834f2af60abe
ashwingit2001 <ashwindivakaran0112@gmail.com>
2025-10-16 14:23:10 +0530

    live host redirect root

--- backend/main.py
+++ backend/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins = [
         "http://localhost:3000", 
-        "https://mood-tune.vercel.app/", 
+        "https://mood-tune.vercel.app", 
         "https://moodtune-l8t8.onrender.com"
     ],
     allow_credentials = True,
